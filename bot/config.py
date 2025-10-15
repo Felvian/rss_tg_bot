db8d45c6af9bc2eaa92b88ee3ea583737b4dbfff
Панкратова Анастасия < @ana>
2025-10-15 10:38:49 +0700

    newposts

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,4 @@
 import os
 BOT_TOKEN   = os.getenv("BOT_TOKEN")
 CHANNEL_ID  = os.getenv("CHANNEL_ID")
-BRIDGE_URL  = 'https://rss-bridge.org/bridge01/?action=display&bridge=TelegramBridge&username=%40{username}&format=Json'
+BRIDGE_URL  = 'https://rss-bridge.org/bridge01/?action=display&bridge=TelegramBridge&username=%40{username}&format=Atom'
